b797e5bf4019e6ffe8f77c37a3679357e6351343
Aldo Mercado <amercado1014@gmail.com>
2018-05-21 09:56:17 -0600

    Setup server file

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
+app.use(bodyParser.json());
+app.use('/', express.static('public'));
+
+app.set('port', process.env.PORT || 3000);
+app.locals.title = 'Photo Trapper Keeper';
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+});
