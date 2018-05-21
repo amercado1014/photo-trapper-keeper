820ebb9574046594ad314ab278a95b88adeb1778
Aldo Mercado <amercado1014@gmail.com>
2018-05-21 12:06:21 -0600

    Add form and displayPhoto function

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,1 +1,15 @@
-alert('Hello');
+$('.add-button').on('click', displayPhoto);
+
+function displayPhoto(event) {
+  event.preventDefault();
+  const photoTitle = $('.title').val();
+  const photoLink = $('.photo-link').val();
+
+  $('.album').prepend(`
+    <article class='photo-container'>
+      <img class='photo' src=${photoLink} alt=${photoTitle}>
+      <h2>${photoTitle}</h2>
+    </article>
+  `);
+}
+
